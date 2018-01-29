5e983e99e221469ad42331c0abdfcacfc27e6551
Sumit Gupta <sumit.gupta@kroger.com>
2018-01-29 10:57:10 -0500

    Modernization: Struts to Spring lab - 1

--- src/main/java/org/superbiz/struts/UserRepository.java
+++ src/main/java/org/superbiz/struts/UserRepository.java
@@ -1,9 +1,18 @@
 package org.superbiz.struts;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public List<User> findAllByFirstName(String firstName);
+    public List<User> findAllByFirstNameOrderByLastName(String firstName);
+
+    @Query("Select * where id between :startId and :endId")
+    public List<User> findWhereIdBetween(Long startId, Long endId);
 
 }
